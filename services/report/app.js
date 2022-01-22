3910ef841f0c42e4a1ed4131207e3fc8aa6e09eb
Diego Barros <diegobsilveira@gmail.com>
2022-01-22 19:01:15 -0300

    Criar serviço de gerar relatórios para entregas

--- services/report/app.js
+++ services/report/app.js
@@ -1,30 +1,57 @@
-const RabbitMQService = require('./rabbitmq-service')
-const path = require('path')
+const RabbitMQService = require('./rabbitmq-service');
+const path = require('path');
 
-require('dotenv').config({ path: path.resolve(__dirname, '.env') })
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
-var report = {}
-async function updateReport(products) {
-    for(let product of products) {
-        if(!product.name) {
-            continue
-        } else if(!report[product.name]) {
-            report[product.name] = 1;
-        } else {
-            report[product.name]++;
-        }
-    }
+var report = {};
+const updateReport = async (products) => {
+	products.forEach((product) => {
+		if (product.name) {
+			if (report[product.name]) report[product.name] = 1;
+			else report[product.name]++;
+		}
+	});
+};
 
-}
+const printReport = async () => {
+	Object.entries(report).forEach(([key, value]) => {
+		console.log(`${key} = ${value} vendas`);
+	});
+};
 
-async function printReport() {
-    for (const [key, value] of Object.entries(report)) {
-        console.log(`${key} = ${value} vendas`);
-      }
-}
+const hasProductsIn = (delivery) =>
+	Array.isArray(delivery.products) && delivery.products.length > 0;
 
-async function consume() {
-    //TODO: Constuir a comunicação com a fila 
-} 
+const processMessage = (msg) => {
+	const delivery = JSON.parse(msg.content);
 
-consume()
+	try {
+		if (hasProductsIn(delivery)) {
+			updateReport(delivery);
+			printReport();
+
+			console.log('✔ SUCCESS: RELATÓRIO GERADO');
+		} else
+			console.warn(
+				'X ERRO: NÃO É POSSÍVEL CRIAR RELATÓRIO PARA UM PEDIDO QUE NÃO POSSUI PRODUTOS'
+			);
+	} catch (error) {
+		console.log(error);
+		console.log(`X ERROR TO PROCESS: ${error.response}`);
+	}
+};
+
+const consume = async () => {
+	console.log(`INSCRITO COM SUCESSO NA FILA: ${process.env.RABBITMQ_QUEUE_NAME}`);
+	try {
+		await (
+			await RabbitMQService.getInstance()
+		).consume(process.env.RABBITMQ_QUEUE_NAME, (msg) => {
+			processMessage(msg);
+		});
+	} catch (error) {
+		console.error({ error });
+	}
+};
+
+consume();
